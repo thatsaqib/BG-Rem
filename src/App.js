fb17e9969c2ee0b2568882a146861d9f2a0397ee
Saqib Asif <devopsaqib@gmail.com>
2026-04-03 23:13:21 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ function App() {
     formData.append('file', selectedFile);
 
     try {
-      const response = await axios.post(process.env.BACKEND_URI, formData, {
+      const response = await axios.post(process.env.BACKEND_URI + '/process', formData, {
         responseType: 'blob',
         timeout: 120000 // 2 minutes for HD
       });
